ee1e10e654bdd01de074782f871dd64aab089e73
Elaiza Endrinal <endrinalkyla8@gmail.com>
2024-01-24 23:36:28 +0800

    Defined the function to print multiplication table

--- Multiplication_Table.py
+++ Multiplication_Table.py
@@ -3,5 +3,9 @@
 # pseudocode
 
 # Define function to print multiplication table
-
+def print_multiplication_table():
+    for i in range (1,11):
+        for j in range (1, 11):
+            result=i*j
+            
 # Print the multiplication table
